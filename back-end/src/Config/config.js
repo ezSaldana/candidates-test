d7478947412608caf048e4aff4d27b92028708bd
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-09-28 23:17:56 -0500

    Changed dev PORT and added local database url to make the connection

--- back-end/src/Config/config.js
+++ back-end/src/Config/config.js
@@ -1,9 +1,14 @@
 // ==============================
 // PORT
 // ==============================
-process.env.PORT = process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 1234;
 
 // ==============================
 // ENVIRONMENT
 // ==============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==============================
+// DATABASE
+// ==============================
+process.env.DB_URL = 'mongodb://localhost:27017/gpactest'
