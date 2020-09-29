af7596dc6bbbc50f628015debec8377cfb655595
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-09-28 23:21:42 -0500

    Using cors to allow requests from anywhere. Using bodyparser to send params in the body. Set a global routes file that contains all the routes and required database connection.

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,22 +1,26 @@
 require('./Config/config');
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // ==============================
 // MIDDLEWARES
 // ==============================
-
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // ==============================
 // ROUTES GLOBAL CONFIG
 // ==============================
-
+app.use(require('./Routes/index'));
 
 // ==============================
 // EXECUTION
 // ==============================
-
+require('./database');
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
