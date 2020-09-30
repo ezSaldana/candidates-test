960e43f83037cd82a27f79b69793aa58f921389b
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-09-29 23:38:56 -0500

    Added icons used in navbar and market (candidates) page and exported 'em in the index file

---
+++ front-end/src/Assets/Icons/index.jsx
@@ -0,0 +1,29 @@
+import LogoIcon from './Logo.svg';
+import CompaniesIcon from './Navbar/Companies.svg';
+import DashboardIcon from './Navbar/Dashboard.svg';
+import JobOrdersIcon from './Navbar/JobOrders.svg';
+import MapIcon from './Navbar/Map.svg';
+import MarketIcon from './Navbar/Market.svg';
+import SendoutsIcon from './Navbar/Sendouts.svg';
+import SProjectsIcon from './Navbar/SProjects.svg';
+import TasksToolIcon from './Navbar/TasksTool.svg';
+import AddIcon from './Market/Add.svg';
+import ArrowDownIcon from './Market/ArrowDown.svg';
+import DotsIcon from './Market/Dots.svg';
+import SearchIcon from './Market/Search.svg';
+
+export {
+  LogoIcon,
+  CompaniesIcon,
+  DashboardIcon,
+  JobOrdersIcon,
+  MapIcon,
+  MarketIcon,
+  SendoutsIcon,
+  SProjectsIcon,
+  TasksToolIcon,
+  AddIcon,
+  ArrowDownIcon,
+  DotsIcon,
+  SearchIcon,
+}
