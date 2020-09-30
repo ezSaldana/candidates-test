5a7c49eb79c0008e3747b8ce216cde3029dc6c26
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-09-30 00:39:22 -0500

    Added nabarLinks as helper to load the list items in the navbar component. It exports an array with the necessary data.

---
+++ front-end/src/Helpers/navbarLinks.jsx
@@ -0,0 +1,21 @@
+import {
+  CompaniesIcon,
+  DashboardIcon,
+  JobOrdersIcon,
+  MapIcon,
+  MarketIcon,
+  SProjectsIcon,
+  SendoutsIcon,
+  TasksToolIcon,
+} from '../Assets/Icons'
+
+export const navbarLinks = [
+  { to: '/dashboard', name: 'Dashboard', icon: DashboardIcon },
+  { to: '/job-orders', name: 'Job Orders', icon: JobOrdersIcon },
+  { to: '/market', name: 'Market', icon: MarketIcon },
+  { to: '/companies', name: 'Companies', icon: CompaniesIcon },
+  { to: '/s-projects', name: 'S. Projects', icon: SProjectsIcon },
+  { to: '/map', name: 'Map', icon: MapIcon },
+  { to: '/tasks-tool', name: 'Tasks Tool', icon: TasksToolIcon },
+  { to: '/sendouts', name: 'Sendouts', icon: SendoutsIcon },
+];
