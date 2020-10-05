243d87ec6e11f8f677cddd930ed42eedf1880dd6
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-10-04 22:14:33 -0500

    Main market page structure, added the add talent form but talents table is missing

--- front-end/src/Pages/Market/Market.jsx
+++ front-end/src/Pages/Market/Market.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Button,
+  Collapse,
+  Grid,
+  Typography,
+  useTheme
+} from '@material-ui/core';
+
+import { AddIcon } from '../../Assets/Icons';
+import AddTalent from '../../Components/AddTalent';
 
 const Market = () => {
+  const [newTalent, setNewTalent] = useState(true);
+  const theme = useTheme();
+
+  const handleAddNewTalent = () => {
+    setNewTalent(!newTalent);
+  }
   return (
-    <div>
-      MARKET PAGE
-    </div>
+    <Grid container direction='column' >
+      <Grid container item justify='space-between' >
+        <Grid item >
+          <Grid container alignItems='center' >
+            <svg height='22px' width='22px' >
+              <ellipse cx='11' cy='11' rx='11' ry='11' fill={theme.palette.red.main} />
+            </svg>
+            <Typography variant='h5' display='inline' style={{ fontFamily: 'Montserrat', fontWeight: '700', lineHeight: '29px', letterSpacing: '0.48px', marginLeft: 13 }} >Market</Typography>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Button
+            variant='contained'
+            style={{ width: 175 }}
+            onClick={handleAddNewTalent}
+            endIcon={<img src={AddIcon} alt='Add new talent icon' />}
+          >
+            Add new talent
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid item style={{padding: '2% 0'}} >
+        <Collapse in={newTalent}>
+          <AddTalent />
+        </Collapse>
+      </Grid>
+      <Grid container item >
+
+      </Grid>
+    </Grid>
   )
 }
 
