0899277e56981a1dbadae3374adb12b388fdfcaa
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-10-04 21:26:57 -0500

    Exported default from approuter

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core';
 import theme from './globalMUIStyles';
-import AppRouter from './Routers/AppRouter';
+import AppRouter from './Routers';
 
 function App() {
   return (
