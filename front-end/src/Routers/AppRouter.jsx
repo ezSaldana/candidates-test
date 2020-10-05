23595290abd50c518473fc609de4cc95b6bd45d3
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-10-04 21:42:07 -0500

    Added styles

--- front-end/src/Routers/AppRouter.jsx
+++ front-end/src/Routers/AppRouter.jsx
@@ -8,14 +8,17 @@ import {
 import Market from '../Pages/Market';
 import Mapa from '../Pages/Map';
 import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer/Footer';
+import Footer from '../Components/Footer';
+import { useTheme } from '@material-ui/core';
 
 const AppRouter = () => {
+  const theme = useTheme();
   return (
     <Router>
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: 'flex', height: '100%'}}>
         <Navbar />
-        <main style={{ flexGrow: 1 }}>
+        <main style={{ flexGrow: 1, padding: '2.5% 5% 1% 2.5%', marginTop: theme.mixins.toolbar.minHeight,
+        marginBottom: theme.mixins.footer.minHeight }}>
           <Switch>
             <Route exact path='/market' component={Market} />
             <Route exact path='/map' component={Mapa} />
