2d39b62fb8671f38be76db28bf2d20eace8a5ccd
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-10-04 21:43:35 -0500

    Added styles to global theme

--- front-end/src/globalMUIStyles.jsx
+++ front-end/src/globalMUIStyles.jsx
@@ -3,13 +3,46 @@ import { createMuiTheme } from '@material-ui/core';
 const montserrat = '"Montserrat", sans-serif';
 
 const theme = createMuiTheme({
+  mixins: {
+    toolbar: {
+      minHeight: '65px',
+    },
+    footer: {
+      minHeight: '40px',
+    },
+    drawer: {
+      width: '140px',
+    }
+  },
   typography: {
+    h1: {
+      color: '#fff',
+    },
+    h2: {
+      color: '#fff',
+    },
+    h3: {
+      color: '#fff',
+    },
+    h4: {
+      color: '#fff',
+    },
+    h5: {
+      color: '#fff',
+    },
+    h6: {
+      color: '#fff',
+    },
     caption: {
       fontFamily: montserrat,
       fontWeight: 300,
       fontSize: '0.75rem',
       lineHeight: '0.938rem',
       letterSpacing: .24,
+      color: '#fff'
+    },
+    button: {
+      textTransform: 'none',
     }
   },
   palette: {
@@ -18,12 +51,25 @@ const theme = createMuiTheme({
       main: '#24292E',
       dark: '#1A1C21',
     },
-    background: {
-      paper: '#24292E',
-    },
     text: {
-      primary: '#fff',
+      primary: '#FFF',
+    },
+    red: {
+      light: '#FF6060',
+      main: '#FF3939',
+      dark: '#FF1212',
     }
+  },
+  custom: {
+    paper: {
+      select: {
+        backgroundColor: '#2B313B',
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 20px',
+        height: '40px',
+      },
+    },
   },
   // OVERRIDE
   overrides: {
@@ -48,9 +94,77 @@ const theme = createMuiTheme({
         },
       }
     },
+    MuiToolbar: {
+      root: {
+        backgroundColor: '#20232B',
+      }
+    },
     MuiDrawer: {
       paperAnchorDockedLeft: {
         borderRight: 'none',
+      }
+    },
+    MuiSelect: {
+      root: {
+        width: '120px',
+        fontSize: '16px',
+        lineHeight: '21px',
+        fontWeight: '300',
+        letterSpacing: '0.32px',
+        backgroundColor: 'rgba(0,0,0,0.001)',
+        '&:focus': {
+          backgroundColor: 'rgba(0,0,0,0.001)',
+        }
+      },
+      iconOpen: {
+        transform: 'rotate(180deg)',
+      },
+    },
+    MuiMenu: {
+      paper: {
+        marginTop: '1%',
+      },
+      list: {
+        paddingTop: 0,
+        paddingBottom: 0,
+        background: '',
+        "& li": {
+          fontWeight: 200,
+          paddingTop: 12,
+          paddingBottom: 12,
+        },
+        "& li:hover": {
+          backgroundColor: '#FF6060',
+        },
+        "& li.Mui-selected": {
+          color: 'white',
+          background: '#FF3939',
+        },
+        "& li.Mui-selected:hover": {
+          backgroundColor: '#FF1212',
+        }
+      }
+    },
+    MuiButton: {
+      contained: {
+        backgroundColor: '#4056F4',
+        borderRadius: '2px',
+        border: '#4056F4 1px solid',
+        color: '#FFF',
+        padding: '7px 11px',
+        transition: '300ms',
+        '&:hover': {
+          backgroundColor: '#6577f6',
+        }
+      },
+      label: {
+        display: 'flex',
+        justifyContent: 'space-around',
+      }
+    },
+    MuiDivider: {
+      root: {
+        backgroundColor: '#7B868D',
       }
     },
     MuiPaper: {
