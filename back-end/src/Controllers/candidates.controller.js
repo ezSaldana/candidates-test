05677c0d161b9c6a889b004a9b62ddbf616b0147
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-10-08 14:26:45 -0500

    Added validation to transform candidate data when its passed in form-urlencoded. Edited the returned response with the just added but populated candidate. Fixed error responses to let the user in the front know what's missing

--- back-end/src/Controllers/candidates.controller.js
+++ back-end/src/Controllers/candidates.controller.js
@@ -14,68 +14,93 @@ const candidatesController = {};
  * the recently inserted location object to get the id and pass it to location prop in candidates
  * model.
  */
-candidatesController.createCandidate = async(req, res) => {
+candidatesController.createCandidate = (req, res) => {
+  if (req.get('Content-type') === 'x-www-form-urlencoded') {
+    console.log('entro al urlencoded');
+    req.body = {...req.body, location: JSON.parse(req.body.location) };
+  }
   // First, create the candidate location.
   // We need the location id to insert the candidate.
-  const { name, degree, position, industry, cellphone, salary, avatar, } = req.body;
-  let { location: address } = req.body;
-  // Location is recieved as JSON, so i have to parse it
-  address = JSON.parse(address);
+  const { name, degree, position, industry, cellphone, salary, avatar, location: address } = req.body;
+  // let { location: address } = req.body;
   // Create model with that location
-  const location = new Location({ coords: { lat: address.lat, lng: address.lng, }, ...address });
   try {
-    const savedUser = await location.save();
-    const candidate = new Candidate({
-      name,
-      position,
-      degree,
-      industry,
-      cellphone,
-      salary,
-      location: savedUser._id,
-      avatar,
-    });
-    candidate.save((err, savedCandidate) => {
+    console.log('1');
+    const location = new Location({ coords: { lat: address.lat, lng: address.lng, }, ...address });
+    console.log('2');
+    location.save((err, savedLocation) => {
       if(err) {
         return res.status(400).json({
           ok: false,
-          msg: 'Candidate wasn\'t added',
-          error: err,
-        })
+          msg: 'Couldn\'t add location',
+          error: {
+            ...err,
+            message: 'Location validation failed: location: Field is required, please select a location that contains at least your region, country and city.'
+          }
+        });
       }
-      res.json({
-        ok: true,
-        msg: 'Candidate added',
-        candidate: savedCandidate,
-      })
+      const candidate = new Candidate({
+        name,
+        position,
+        degree,
+        industry,
+        cellphone,
+        salary,
+        location: savedLocation._id,
+        avatar,
+      });
+      candidate.save(async(err, savedCandidate) => {
+        if (err) {
+          return res.status(400).json({
+            ok: false,
+            msg: 'Candidate wasn\'t added',
+            error: err,
+          })
+        }
+        const schemas = ['degree', 'position', 'industry', 'location'];
+        for (item of schemas) {
+          item !== 'location' ?
+            savedCandidate = await savedCandidate.populate(item, 'name').execPopulate() :
+            savedCandidate = await savedCandidate.populate(item).execPopulate()
+        }
+        res.json({
+          ok: true,
+          msg: 'Candidate Added Succesfully',
+          candidate: savedCandidate,
+        })
+      });
     });
-  } catch (e) {
-    console.log(e);
+  } catch (err) {
+    res.json({
+      ok: false,
+      message: 'Unhandled Error',
+      error: err,
+    })
   }
 }
 
 candidatesController.getAllCandidates = (req, res) => {
-  Candidate.find({status: true})
-  .populate('degree', 'name')
-  .populate('position', 'name')
-  .populate('industry', 'name')
-  .populate('location')
-  .sort({addedDate: -1})
-  .exec((error, candidates) => {
-    if (error) {
-      return res.status(400).json({
-        ok: false,
-        msg: 'Couldn\'t find any candidates',
-        error: error.errors
+  Candidate.find({ status: true })
+    .populate('degree', 'name')
+    .populate('position', 'name')
+    .populate('industry', 'name')
+    .populate('location')
+    .sort({ addedDate: -1 })
+    .exec((error, candidates) => {
+      if (error) {
+        return res.status(400).json({
+          ok: false,
+          msg: 'Couldn\'t find any candidates',
+          error: error.errors
+        });
+      }
+      res.json({
+        ok: true,
+        msg: 'Candidates found',
+        count: candidates.length,
+        candidates,
       });
-    }
-    res.json({
-      ok: true,
-      msg: 'Candidates found',
-      count: candidates.length,
-      candidates,
     });
-  });
 }
 
 module.exports = candidatesController;
