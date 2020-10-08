76b14e12b18ed595a4c912b4191c916dc2fa68e1
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-10-08 12:51:49 -0500

    Added redux store provider

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './Redux/Store';
 import { MuiThemeProvider } from '@material-ui/core';
 import theme from './globalMUIStyles';
 import AppRouter from './Routers';
@@ -6,7 +8,9 @@ import AppRouter from './Routers';
 function App() {
   return (
     <MuiThemeProvider theme={theme} >
-      <AppRouter />
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
     </MuiThemeProvider>
   );
 }
