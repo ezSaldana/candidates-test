af33f665b2cf3e7b6de52311fad74915003e9f4e
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-10-08 14:12:57 -0500

    Added candidatesList component with styles and main functionality, it loads all the users in the DB and everytime you add a new one, it's shown in the first position.

---
+++ front-end/src/Components/CandidatesList/CandidatesList.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Paper, Typography } from '@material-ui/core'
+
+import { setActiveMapCandidate } from '../../Redux/Actions/Map';
+import { useStyles } from '../../Hooks';
+import styles from './styles';
+
+
+const CandidatesList = ({ list }) => {
+  const classes = useStyles(styles);
+  const dispatch = useDispatch();
+  const { activeCandidate: active } = useSelector(state => state.map);
+
+  const handleItemSelect = (id, coords, zoom) => () => dispatch(setActiveMapCandidate({ id, coords, zoom }));
+
+  return (
+    <Paper className={classes.paper}>
+      <List className={classes.list}>
+        {
+          list.map((candidate) => (
+            <ListItem
+              button
+              id={candidate.cellphone}
+              selected={active.id === candidate._id}
+              onClick={handleItemSelect(
+                candidate._id,
+                [candidate.location.coords.lng, candidate.location.coords.lat],
+                [14]
+              )}
+              classes={{ button: classes.listItem }}
+              divider
+            >
+              <ListItemAvatar>
+                <Avatar
+                  src={candidate.avatar}
+                  alt={`${candidate.name} Avatar`}
+                  className={classes.avatar}
+                />
+              </ListItemAvatar>
+              <ListItemText
+                primary={candidate.name}
+                secondary={
+                  <>
+                    <Typography
+                      variant='body2'
+                      component='span'
+                      display='block'
+                      paragraph
+                    >
+                      {candidate.degree.name}
+                    </Typography>
+                    <Typography variant='caption' align='justify' >
+                      {candidate.location.address}
+                    </Typography>
+                  </>
+                }
+              />
+            </ListItem>
+          ))
+        }
+      </List>
+    </Paper>
+  )
+}
+
+export default CandidatesList
---
+++ front-end/src/Components/CandidatesList/styles.jsx
@@ -0,0 +1,21 @@
+export default (theme) => ({
+  avatar: {
+    width: '5rem',
+    height: '5rem',
+    marginRight: 20,
+  },
+  paper: {
+    minHeight: '100%',
+    maxHeight: 'calc(100% - 20px)',
+    height: '100%',
+    overflow: 'auto',
+  },
+  list: {
+    padding: 0,
+  },
+  listItem: {
+    '&:hover': {
+      backgroundColor: '#585866',
+    }
+  }
+});
