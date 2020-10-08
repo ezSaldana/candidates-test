5552ea3ab5d5e86e6ae671eb735ec748b0a8bd5f
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2020-10-08 12:40:03 -0500

    Added candidates types and map types

---
+++ front-end/src/Types/types.jsx
@@ -0,0 +1,11 @@
+export default {
+  candidateAdd: '[candidate] add',
+  candidatesSetList: '[candidate] setCandidatesList',
+  candidateAddSetSelectsOpts: '[candidate] loadAddSelectsOpts',
+  candidatesAddSetSelectSelected: '[candidate] setAddSelectSelected',
+  candidatesAddSetSelectClearSelected: '[candidate] setAddSelectClearSelected',
+  candidatesAddSetLocation: '[candidate] setAddLocation',
+  candidatesAddClearLocation: '[candidate] setAddClearLocation',
+
+  mapSetActiveCandidate: '[map] setActiveCandidate',
+}
